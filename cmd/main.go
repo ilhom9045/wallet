2d40100ab4949bcf6fd746ce23325b3112f6aadd
ilhom9045 <fozilov.7459045@gmail.com>
2020-10-16 18:07:59 +0500

    ?

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/ilhom9045/wallet/pkg/wallet"
 	"log"
-	"os"
 )
 
 func main() {
@@ -19,8 +18,7 @@ func main() {
 	favorite, _ = svc.FavoritePayment(payment.ID, "megafon")
 	svc.PayFromFavorite(favorite.ID)
 	dir, _ := svc.GetDir()
-	dir += "/export"
-	os.MkdirAll(dir,0777)
+	//os.MkdirAll(dir,0777)
 	svc.ExportToFile("data/export.txt")
 	svc.ImportFromFile("data/export.txt")
 	svc.Export(dir)
@@ -28,4 +26,65 @@ func main() {
 	if err != nil {
 		log.Print(err)
 	}
+
+	//svc := &wallet.Service{}
+	//accountTest , err := svc.RegisterAccount("+992000000001")
+	//if err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+	//
+	//err = svc.Deposit(accountTest.ID, 100_000_00)
+	//if err != nil {
+	//	switch err {
+	//	case wallet.ErrAmountMustBePositive:
+	//		fmt.Println("Сумма должна быть положительной")
+	//	case wallet.ErrAccountNotFound:
+	//		fmt.Println("Аккаунт пользователя не найден")
+	//	}
+	//	return
+	//}
+	//fmt.Println(accountTest.Balance)
+	//
+	//err = svc.Deposit(accountTest.ID, 200_000_00)
+	//if err != nil {
+	//	switch err {
+	//	case wallet.ErrAmountMustBePositive:
+	//		fmt.Println("Сумма должна быть положительной")
+	//	case wallet.ErrAccountNotFound:
+	//		fmt.Println("Аккаунт пользователя не найден")
+	//	}
+	//	return
+	//}
+	//fmt.Println(accountTest.Balance)
+	//
+	//
+	//newPay, err := svc.Pay(accountTest.ID,10_000_00,"auto")
+	//newPay, err = svc.Pay(accountTest.ID,10_000_00,"food")
+	//newPay, err = svc.Pay(accountTest.ID,10_000_00,"animal")
+	//newPay, err = svc.Pay(accountTest.ID,10_000_00,"car")
+	//newPay, err = svc.Pay(accountTest.ID,10_000_00,"restaurent")
+	//fmt.Println(accountTest.Balance)
+	//fmt.Println(newPay)
+	//fmt.Println(err)
+	//
+	//fav, errFav := svc.FavoritePayment(newPay.ID, "Babilon")
+	//fmt.Println(errFav)
+	//fmt.Println(fav)
+	//
+	//wd, err := os.Getwd()
+	//if err != nil {
+	//	log.Print(err)
+	//	return
+	//}
+	//err = svc.Export(wd)
+	//if err != nil {
+	//	return
+	//}
+	//err = svc.Import(wd)
+	//if err != nil {
+	// 	log.Print(err)
+	// 	return
+	//}
+
 }
